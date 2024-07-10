c749ec1c276cb1051bbe97ff76e9508efc2a275a
Iftekhar Turja <102432702+arcaus@users.noreply.github.com>
2024-07-11 01:01:58 +0600

    Update 2a.c
    Refactoring code for better readability.

--- Lab 01 - 02/Task 2/2a.c
+++ Lab 01 - 02/Task 2/2a.c
@@ -1,64 +1,52 @@
-
+// written by arcaus
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-// addition
 
 float add(float x, float y)
 {
     return x + y;
 }
 
-// subtraction
-
 float sub(float x, float y)
 {
     return x - y;
 }
-
-// multiplication
 
 float mul(float x, float y)
 {
     return x * y;
 }
 
-// main function
-
 int main()
 {
     float a, b;
-    printf("Enter the first number: ");
-    scanf("%f", &a);
-    printf("Enter the second number: ");
-    scanf("%f", &b);
-    if (a > b)
-    {
-        printf("Subtraction: %.3f\n", sub(a, b));
-    }
-    else if (a < b)
-    {
-        printf("Addition: %.3f\n", add(a, b));
-    }
-    else if (a == b)
-    {
-        printf("Multiplication: %.3f\n", mul(a, b));
-    }
-
-    // loop the program to run again
-
-    printf("\nDo you want to run the program again? (y/n): ");
     char c;
-    scanf(" %c", &c);
-    if (c == 'y' || c == 'Y')
+
+    do
     {
-        main();
-    }
-    else
-    {
-        printf("\nThank you for using the program!\n");
-    }
+        printf("Enter the first number: ");
+        scanf("%f", &a);
+        printf("Enter the second number: ");
+        scanf("%f", &b);
+
+        if (a > b)
+        {
+            printf("Subtraction: %.3f\n", sub(a, b));
+        }
+        else if (a < b)
+        {
+            printf("Addition: %.3f\n", add(a, b));
+        }
+        else if (a == b)
+        {
+            printf("Multiplication: %.3f\n", mul(a, b));
+        }
+
+        printf("\nDo you want to run the program again? (y/n): ");
+        scanf(" %c", &c);
+    } while (c == 'y' || c == 'Y');
+
+    printf("\nThank you for using the program!\n");
     return 0;
 }
