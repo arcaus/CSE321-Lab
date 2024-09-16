edf9fbf9751ae2fa4d614bcabdd995f7ff980338
Iftekhar Turja <102432702+arcaus@users.noreply.github.com>
2024-09-16 16:09:08 +0600

    Update task5.c
    simplified code

--- Lab 03/System Call Tasks/task5.c
+++ Lab 03/System Call Tasks/task5.c
@@ -1,55 +1,33 @@
-
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <string.h>
 
-/*
-Question:
-Write a program in c that the parent process will create one child process and 3 grandchild
-processes and print their IDs
-Output: 1. Parent process ID : 0
-2. Child process ID : ...
-3. Grand Child process ID: ...
-4. Grand Child process ID: ...
-5. Grand Child process ID: ...
-*/
-
-// main function
 int main()
 {
-    pid_t pid, pid1, pid2, status;
+    pid_t pid;
     pid = fork();
     if (pid == 0)
     {
-        pid1 = fork();
-        if (pid1 > 0)
+        printf("Child process ID: %d\n", getpid());
+        for (int i = 0; i < 3; i++)
         {
-            printf("Child process ID : %d\n", getpid());
-        }
-        else
-        {
-            pid2 = fork();
-            if (pid2 > 0)
-            {
-                fork();
-                printf("Grand Child process ID: %d\n", getpid());
-            }
-            else
+            pid = fork();
+            if (pid == 0)
             {
                 printf("Grand Child process ID: %d\n", getpid());
+                break;
             }
         }
     }
     else if (pid > 0)
     {
-        printf("Parent process ID : %d\n", getpid());
+        printf("Parent process ID: %d\n", getpid());
     }
+
     else
     {
-        perror("Error in fork");
+        printf("Fork failed\n");
     }
+
     return 0;
 }
